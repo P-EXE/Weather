7aca389751b951752dc921b35395e2c808baf9ee
alice <alice@p-exe.com>
2026-06-28 13:34:35 +0200

    flakey

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A Nix-flake-based C# development environment";
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+  outputs = { self, ... }@inputs:
+  let
+    inherit (inputs.nixpkgs) lib;
+    supportedSystems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ];
+    forEachSupportedSystem = f: lib.genAttrs supportedSystems (
+      system: f {
+        pkgs = import inputs.nixpkgs { inherit system; };
+      }
+    );
+  in {
+    devShells = forEachSupportedSystem ({ pkgs }: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          dotnet-sdk_10
+          omnisharp-roslyn
+          mono
+          msbuild
+        ];
+      };
+    });
+  };
+}
